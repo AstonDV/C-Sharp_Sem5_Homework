9c42eedc59be0341d8bcd2ee733d0c2fc582ec97
AstonDV <astondv.kms@gmail.com>
2023-01-28 09:51:20 +1000

    End task 34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);    
+    }
+    return res;
+}
+
+bool EvenCheckRule(int elementArray)
+{
+    if(elementArray % 2 == 0) return true;
+    else return false;
+}
+
+int CountElements(int[] ourArray)
+{
+    int count = 0;
+
+    foreach(int elem in ourArray)
+    {
+        if (EvenCheckRule(elem)) count++;
+    }
+
+    return count;
+}
+
+void PrintResult(int[] array, int count)
+{
+    Console.WriteLine($"В массиве ({String.Join(", ", array)}) {count} чётных элементов");
+}
+
+void main()
+{
+    int[] array = GetArray(10, 100, 1000);
+    int elementsCounted = CountElements(array);
+    PrintResult(array, elementsCounted);
+}
+
+main();
