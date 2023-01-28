fc0af731a52bf4a324c434c0588f8fa2be5fff66
AstonDV <astondv.kms@gmail.com>
2023-01-28 21:34:14 +1000

    End task *

--- Task+/Program.cs
+++ Task+/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Разобраться с алгоритмом сортировки методом пузырька. Реализовать невозрастающую сторировку.
+// [3, 0, 2, 4, -1] -> [4, 3, 2, 0, -1]
+// [1,2,2,3,2] -> [3, 2, 2, 2, 1]
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);    
+    }
+    return res;
+}
+
+int InputArgumentSorting()
+{
+    Console.WriteLine("Каким образом вы хотите отсортировать массив?");
+    Console.WriteLine($"1. Сортировка по возрастанию");
+    Console.WriteLine($"2. Сортировка по убыванию");
+    Console.Write("Выберите метод сортировки (1 или 2): ");
+    int argumentSorting = int.Parse(Console.ReadLine()!);
+    return argumentSorting;
+}
+
+int[] BubbleSort(int[] array, int argument)
+{
+    int temp;
+
+    if(argument == 1)
+    {
+        for(int i = 0; i < array.Length; i++)
+        {
+            for(int j = i + 1; j < array.Length; j++)
+            {
+                if(array[i] > array[j])
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+    else
+    {
+       for(int i = 0; i < array.Length; i++)
+        {
+            for(int j = i + 1; j < array.Length; j++)
+            {
+                if(array[i] < array[j])
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array; 
+    }
+    
+}
+
+void PrintResult(int[] array)
+{
+    Console.WriteLine($"{String.Join(", ", array)}");
+}
+
+void main()
+{
+    Console.Clear();
+    int[] array = GetArray(40, 0, 100);
+    Console.WriteLine("Заданный массив:");
+    PrintResult(array);
+    int argument = InputArgumentSorting();
+    if(argument != 1 && argument != 2)
+    {
+        Console.WriteLine("Ошибка! Введенного метода сортировки не существует!");
+    }
+    else
+    {
+        BubbleSort(array, argument);
+        if (argument == 1) Console.WriteLine("Массив после сортировки по возрастанию:");
+        else Console.WriteLine("Массив после сортировки по убыванию:");
+        PrintResult(array);
+    }
+}
+
+main();
--- Task38/Program.cs
+++ Task38/Program.cs
@@ -35,27 +35,28 @@ int SearchMin(double[] array)
 
 void PrintArray(double[] array)
 {
+    Console.WriteLine("Заданный массив элементов:");
     int count = array.Length - 1;
 
     for (int i = 0; i < count; i++)
     {
-        Console.Write($"{array[i]:f4}, ");
+        Console.Write($"{array[i]:f2},  ");
     }
-    Console.Write($"{array[count++]:f4}");
+    Console.Write($"{array[count++]:f2}");
     Console.WriteLine();
 }
 
 void PrintResult(double[] array, int indexMinNum, int indexMaxNum)
 {
-    // Console.WriteLine($"{String.Join(", ", array)}");    
-    Console.WriteLine(array[indexMinNum]);
-    Console.WriteLine(array[indexMaxNum]);
-    Console.WriteLine(array[indexMinNum] + array[indexMaxNum]);
-    Console.WriteLine($"{array[indexMinNum]:f4}");
+    double result = array[indexMinNum] + array[indexMaxNum];
+    Console.WriteLine($"Значение минимального элемента равно {array[indexMinNum]:f2}");
+    Console.WriteLine($"Значение максимального элемента равно {array[indexMaxNum]:f2}");
+    Console.WriteLine($"Сумма максимального и минимального элемента равна {result:f2}");
 }
 
 void main()
 {
+    Console.Clear();
     double[] array = GetArray(10, 1, 100);
     int indexMaxNum = SearchMax(array);
     int indexMinNum = SearchMin(array);
